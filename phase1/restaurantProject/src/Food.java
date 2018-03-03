e5e6e242a9d729523383d2d1f779983ad04f1af4
Leon <leoncyao@gmail.com>
2018-03-03 14:45:22 -0500

    Completed Food.java

--- phase1/restaurantProject/src/Food.java
+++ phase1/restaurantProject/src/Food.java
@@ -1,16 +1,40 @@
 /*
 Represents a food
  */
+import java.util.HashMap;
 import java.util.Map;
 
 public class Food{
     //Make list of types of ingredients
     Map<String, Integer> ingredients;
     private float price;
+    private String name;
 
-    public void addIngredient(){}
+    public Food(String name, float price, Map<String, Integer> ingredients){
+        this.name = name;
+        this.price = price;
+        this.ingredients = ingredients;
+    }
 
-    public void removeIngredient(){}
+    public void addIngredient(String ingredientName, int ingredientQuantity){
+        if (ingredients.containsKey(ingredientName)){
+            int originalQuantity = ingredients.get(ingredientName);
+            ingredients.put(ingredientName, originalQuantity + ingredientQuantity);
+        }else{
+            ingredients.put(ingredientName, ingredientQuantity);
+        }
+    }
+
+    public void removeIngredient(String ingredientName, int ingredientQuantity){
+        if (ingredients.containsKey(ingredientName)) {
+            int originalQuantity = ingredients.get(ingredientName);
+            if (ingredientQuantity >= originalQuantity) {
+                ingredients.remove(ingredientName);
+            } else {
+                ingredients.put(ingredientName, originalQuantity - ingredientQuantity);
+            }
+        }
+    }
 
     public void setPrice(float price) {
         this.price = price;
@@ -19,5 +43,8 @@ public class Food{
     public float getPrice() { return price; }
 
     //This is here so when we make an order we can give a copy of the Food
-    public Food(Food another){}
+    public Food(Food another){
+        Map<String, Integer> ingredientsCopy = new HashMap<String, Integer>(another.ingredients);
+        Food newFood = new Food(another.name, another.price, ingredientsCopy);
+    }
 }
