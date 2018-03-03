d82a499467feef4496d94d67fbbee026affcaee2
Leon <leoncyao@gmail.com>
2018-03-03 15:40:11 -0500

    Updated Cook.java and Server.java

--- phase1/restaurantProject/src/Cook.java
+++ phase1/restaurantProject/src/Cook.java
@@ -1,10 +1,30 @@
-public class Cook {
 
-    public void acceptOrder(OrderManager manager, Order order){
-        manager.acceptOrder(order);
+import sun.awt.image.ImageWatched;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Cook {
+    private static int classIdNumber = 1;
+    private int idNumber;
+    private Order currentOrder;
+
+    public Cook(Kitchen kitchen){
+        this.idNumber = classIdNumber;
+        classIdNumber += 1;
+
     }
 
-    public void submitOrder(OrderManager manager, Order order){
-        manager.orderIsCooked(order);
+    public void acceptOrder(OrderManager manager){
+        manager.acceptOrder(currentOrder);
+    }
+
+    public void submitOrder(OrderManager manager){
+        manager.orderIsCooked(currentOrder);
+    }
+
+    public void chooseOrder(OrderManager manager){
+        LinkedList<Order> pendingOrders = manager.getPendingOrders();
+        currentOrder = pendingOrders.getFirst();
     }
 }
